160e733b137d4980f4ad5ee2bcc351c381920657
grscheller <geoffrey@scheller.com>
2026-08-19 16:08:22 -0600

    Updates for bin/sourced/configure_xdg_dirs.sh & bin/nvimInstall

--- bin/sourced/configure_xdg_dirs.sh
+++ bin/sourced/configure_xdg_dirs.sh
@@ -1,26 +1,34 @@
 ## Setup XDG Desktop folder locations
 #
-# grscheller/dotfiles uses these names in its setup scripts.
+# grscheller/nvim-dotfiles uses these names in its setup scripts.
 #
-# - defaults to standard locations if not already defined
-# - tries to ensures directories exist
-# - failure to create will be indicated via stderr
+# - defaults to standard locations on Linux if not already defined
+# - on Windows 11 set to the corresponding Windows locations
+#   - default Windows locations already exist, their ACL's are not the user's to change
+# - can be overridden for multiple configurations
 #
 # shellcheck shell=sh
 
-# Script exclusively use these names
+if test "$XDG_CONFIG_HOME" = ""
+then
+    _ensure_xdg_dirs_exist=no
+else
+    _ensure_xdg_dirs_exist=yes
+fi
+
 if test "$OS" = Windows_NT
 then
+    # Window 11
     _local_app_data="$(cygpath -u "$LOCALAPPDATA")"
     : "${XDG_CONFIG_HOME:=$_local_app_data}"
     : "${XDG_DATA_HOME:=$_local_app_data}"
     : "${XDG_STATE_HOME:=$_local_app_data}"
     : "${XDG_CACHE_HOME:=$_local_app_data/Temp}"
-    unset _local_app_data
     # shellcheck disable=SC2034  # consumed by nvimInstall after sourcing
     nvim_data_dir=nvim-data
 else
-    # Can be overridden for multiple configurations
+    # Linux
+    _ensure_dirs_exist=yes
     : "${XDG_CONFIG_HOME:=$HOME/.config}"
     : "${XDG_DATA_HOME:=$HOME/.local/share}"
     : "${XDG_STATE_HOME:=$HOME/.local/state}"
@@ -30,12 +38,17 @@ else
 fi
 export XDG_CONFIG_HOME XDG_DATA_HOME XDG_STATE_HOME XDG_CACHE_HOME
 
-# Ensure these folder exists before installation.
-ensure_dir "$XDG_CONFIG_HOME" >&2
-chmod 0755 "$XDG_CONFIG_HOME"
-ensure_dir "$XDG_DATA_HOME" >&2
-chmod 0755 "$XDG_DATA_HOME"
-ensure_dir "$XDG_STATE_HOME" >&2
-chmod 0755 "$XDG_STATE_HOME"
-ensure_dir "$XDG_CACHE_HOME" >&2
-chmod 0755 "$XDG_CACHE_HOME"
+if test "_ensure_dirs_exist" = "yes"
+then
+    ensure_dir "$XDG_CONFIG_HOME" >&2
+    chmod 0755 "$XDG_CONFIG_HOME"
+    ensure_dir "$XDG_DATA_HOME" >&2
+    chmod 0755 "$XDG_DATA_HOME"
+    ensure_dir "$XDG_STATE_HOME" >&2
+    chmod 0755 "$XDG_STATE_HOME"
+    ensure_dir "$XDG_CACHE_HOME" >&2
+    chmod 0755 "$XDG_CACHE_HOME"
+fi
+
+unset _ensure_dirs_exist
+unset _local_app_data
